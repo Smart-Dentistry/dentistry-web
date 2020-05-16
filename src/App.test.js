be697df3151cdc137bb9dba21960e4e266267984
M <m@mathsistor.com>
2020-05-15 22:45:53 -0500

    Display users from API

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App'
 
 test('display page header', () => {
   const { getByText } = render(<App />)
-  const linkElement = getByText(/Dentistry Web UI/i)
+  const linkElement = getByText(/Loading.../i)
   expect(linkElement).toBeInTheDocument()
 })
