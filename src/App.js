93c87a465b4d611598e787f06ae641a47ead3b18
M <m@mathsistor.com>
2020-05-20 20:18:58 -0500

    Create Routes for home page and 404 page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 import Home from './Home'
 
 import './App.css'
 
 function App () {
-  return <Home/>
+  return (
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route render={() => <h2>404 Page Not Found</h2>}/>
+    </Switch>
+  )
 }
 
 export default App
