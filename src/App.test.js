eaa226c70e45292511e7d80891ed0883f4f6c1aa
M <m@mathsistor.com>
2020-05-20 20:26:14 -0500

    Fix broken test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { render } from '@testing-library/react'
 import App from './App'
 
 test('display page header', () => {
-  const { getByText } = render(<App />)
+  const { getByText } = render(
+    <Router>
+      <App />
+    </Router>
+  )
   const linkElement = getByText(/Loading.../i)
   expect(linkElement).toBeInTheDocument()
 })
