c60ebc6b0d6af58e7839922efcc59a19088ffa43
M <m@mathsistor.com>
2020-05-20 20:22:48 -0500

    Create silly component Admin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from './Home'
+import Admin from './Admin'
 
 import './App.css'
 
@@ -8,6 +9,7 @@ function App () {
   return (
     <Switch>
       <Route exact path='/' component={Home} />
+      <Route exact path='/admin' component={Admin} />
       <Route render={() => <h2>404 Page Not Found</h2>}/>
     </Switch>
   )
