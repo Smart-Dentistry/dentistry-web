68e2f66266f37355e99c80696d20a850129c0bd4
M <m@mathsistor.com>
2020-05-28 22:18:03 -0500

    Set up auto-detection of language

--- src/i18n.js
+++ src/i18n.js
@@ -3,29 +3,30 @@ import detector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
 import translationES from './locales/es/translation.json'
+import translationEN from './locales/en/translation.json'
 
 // the translations
 // (tip move them in a JSON file and import them)
 const resources = {
   es: {
     translation: translationES
+  },
+  en: {
+    translation: translationEN
   }
 }
-console.log(detector)
+const detection = {
+  order: ['navigator', 'localStorage'],
+  lookupQuerystring: 'lng',
+  caches: ['localStorage']
+}
 
 i18n
   .use(detector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'en',
-    fallbackLng: 'en',
-
-    keySeparator: false, // we do not use keys in form messages.welcome
-
-    interpolation: {
-      escapeValue: false // react already safes from xss
-    }
+    detection
   })
 
 export default i18n
