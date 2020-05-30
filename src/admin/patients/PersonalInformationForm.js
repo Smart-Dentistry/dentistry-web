2dc355cf8d4070edab34818245bf5aec01e8b48f
M <m@mathsistor.com>
2020-05-29 23:11:26 -0500

    Remove needless elements in object for message validation

--- src/admin/patients/PersonalInformationForm.js
+++ src/admin/patients/PersonalInformationForm.js
@@ -18,17 +18,7 @@ const inputLayout = {
 }
 const validateMessages = {
   // eslint-disable-next-line
-  required: '${label} is required!',
-  types: {
-    // eslint-disable-next-line
-    email: '${label} is not validate email!',
-    // eslint-disable-next-line
-    number: '${label} is not a validate number!'
-  },
-  number: {
-    // eslint-disable-next-line
-    range: '${label} must be between ${min} and ${max}'
-  }
+  required: '${label} is required!'
 }
 
 const PersonalInformationForm = ({ next }) => {
