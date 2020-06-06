ac010b61d75aeb9f849978698ca67f563af3d0b8
Nanci <sikabarca@gmail.com>
2020-06-05 23:39:20 -0500

    Add practitioners

--- src/admin/patients/GeneralPractioners.js
+++ src/admin/patients/GeneralPractioners.js
@@ -7,7 +7,8 @@ import {
   Button,
   Card,
   Typography,
-  Tooltip
+  Tooltip,
+  Space
 } from 'antd'
 import { PlusOutlined, DeleteFilled } from '@ant-design/icons'
 import PropTypes from 'prop-types'
@@ -19,14 +20,15 @@ const inputLayout = {
 }
 
 const GeneralPractitioners = ({ generalPractitioners, setGeneralPractitioners }) => {
-  const addGeneralPractitioner = () => {
-    // console.log(form.getFieldsValue())
-    // setGeneralPractitioners(generalPractitioners.concat([{
-    //   name: form.getFieldsValue().practitionerName,
-    //   phone: form.getFieldsValue().practitionerPhone,
-    //   disease: form.getFieldsValue().practitionerDisease
-    // }]))
-    // form.setFieldsValue({ practitionerName: null, practitionerPhone: null, practitionerDisease: null })
+  const [form] = Form.useForm()
+  const onFinish = values => {
+    console.log(values)
+    setGeneralPractitioners([...generalPractitioners, {
+      name: values.practitionerName,
+      phone: values.practitionerPhone,
+      disease: values.practitionerDisease
+    }])
+    form.setFieldsValue({ practitionerName: null, practitionerPhone: null, practitionerDisease: null })
   }
   return (
     <>
@@ -35,41 +37,43 @@ const GeneralPractitioners = ({ generalPractitioners, setGeneralPractitioners })
           <Title level={4}>General practitioners</Title>
         </Col>
       </Row>
-      <Row gutter={16}>
-        <Col offset={6} span={4}>
-          <Form.Item {...inputLayout} name='practitionerName' label='Name'>
-            <Input />
-          </Form.Item>
-        </Col>
-        <Col span={4}>
-          <Form.Item {...inputLayout} name='practitionerPhone' label='Phone'>
-            <PhoneInput className='telephone-input' />
-          </Form.Item>
-        </Col>
-        <Col span={4}>
-          <Form.Item {...inputLayout} name='practitionerDisease' label='Disease'>
-            <Input />
-          </Form.Item>
-        </Col>
-      </Row>
-      <Row>
-        <Col offset={6} span={3}>
-          <Tooltip title="Add name and disease">
-            <Button type='primary' style={{ marginBottom: 25 }} onClick={() => addGeneralPractitioner()}><PlusOutlined />Add</Button>
-          </Tooltip>
-        </Col>
-      </Row>
+      <Form onFinish={onFinish} form={form} layout='vertical'>
+        <Row gutter={16}>
+          <Col offset={6} span={4}>
+            <Form.Item {...inputLayout} name='practitionerName' label='Name'>
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={4}>
+            <Form.Item {...inputLayout} name='practitionerPhone' label='Phone'>
+              <PhoneInput className='telephone-input' />
+            </Form.Item>
+          </Col>
+          <Col span={4}>
+            <Form.Item {...inputLayout} name='practitionerDisease' label='Disease'>
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col offset={6} span={3}>
+            <Tooltip title="Add name and disease">
+              <Button htmlType="submit" type='primary' style={{ marginBottom: 25 }}><PlusOutlined />Add</Button>
+            </Tooltip>
+          </Col>
+        </Row>
+      </Form>
       <Row gutter={[16, 16]}>
-        <Col span={12} offset={6}>
+        <Space direction='vertical' style={{ width: '100%' }}>
           {generalPractitioners.map((item) =>
-            <>
+            <Col span={12} offset={6} key={item}>
               <Card size='small' title={item.name} key={item} extra={<a href='#'><DeleteFilled style={{ color: 'rgba(229, 59, 50, 1)' }} /></a>}>
                 <p>Phone: {item.phone}</p>
                 <p>Disease: {item.disease}</p>
               </Card>
-            </>
+            </Col>
           )}
-        </Col>
+        </Space>
       </Row>
 
     </>
