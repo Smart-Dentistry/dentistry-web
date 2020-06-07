71e33ac21c327f9d9a82432c133121ea2e2a52a9
M <m@mathsistor.com>
2020-06-06 22:09:25 -0500

    Add testing for patientReducer

---
+++ src/admin/__tests__/patients/patientReducer.js
@@ -0,0 +1,28 @@
+import patientReducer from '../../patients/patientReducer'
+
+const patients = [{ key: 1, firstName: 'John' }, { key: 2, firstName: 'Jeff' }]
+
+describe('patientReducer', () => {
+  let newState
+
+  beforeEach(() => {
+    newState = patientReducer([], { type: 'LOAD', patients })
+  })
+
+  it('loads patients', () => {
+    expect(newState).toBe(patients)
+  })
+
+  it('adds new patient', () => {
+    const patient = { key: 3, firstName: 'Peter' }
+    newState = patientReducer(newState, { type: 'ADD', patient: patient })
+
+    expect(newState).toHaveLength(3)
+    expect(newState).toContain(patient)
+  })
+  it('raises exception', () => {
+    expect(() => {
+      newState = projectReducer(newState, { type: 'UNKNOWN' })
+    }).toThrow(Error)
+  })
+})
