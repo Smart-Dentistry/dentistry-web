dde6c4edcf27003fa77d06d5250064e0a4a6acd6
M <m@mathsistor.com>
2020-06-12 00:03:51 -0500

    Implement EditPatient compopnent

--- src/admin/patients/EditPatient.js
+++ src/admin/patients/EditPatient.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useReducer, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import useAxios from 'axios-hooks'
+
+import PatientStepForms from './PatientStepForms'
+
+import newPatientReducer from './newPatientReducer'
 
 const EditPatient = () => {
+  const { key } = useParams()
+  const [patient, dispatchPatient] = useReducer(newPatientReducer, {})
+  const [{ data, loading, error }] = useAxios({
+    url: `${process.env.REACT_APP_API_URL}/patients/${key}/`
+  })
+  useEffect(() => { if (data) dispatchPatient({ type: 'UPDATE', updatedValues: data }) }, [data])
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
+
   return (
-    <h1>
-      Edit Patient
-    </h1>
+    <PatientStepForms
+      patient={patient}
+      dispatchPatient={dispatchPatient}
+    />
   )
 }
 
