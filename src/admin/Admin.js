0a69a115d836cf9713cd5d2229a1a1d1494673e7
M <m@mathsistor.com>
2020-06-16 00:17:55 -0500

    Log out and remove tokens from local storage

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
-import { Layout, Breadcrumb } from 'antd'
+import { Layout, Breadcrumb, Menu } from 'antd'
+
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
 import AdminContent from './AdminContent'
 import Sidebar from './Sidebar'
@@ -9,13 +12,37 @@ import './Admin.sass'
 
 const { Header, Content, Footer } = Layout
 
+// const menu = (
+//   <Menu>
+//     <Menu.Item>
+//       <Button>
+//         Profile
+//       </Button>
+//     </Menu.Item>
+//     <Menu.Item>
+//       <Button>
+//         Settings
+//       </Button>
+//     </Menu.Item>
+//     <Menu.Item>
+//       <Button>
+//         Log Out
+//       </Button>
+//     </Menu.Item>
+//   </Menu>
+// )
+
 const Admin = () => {
   const [layoutWidth, setLayoutWidth] = useState('200px')
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sidebar setLayoutWidth={setLayoutWidth} />
       <Layout className='site-layout' style={{ paddingLeft: layoutWidth }}>
-        <Header className='site-layout-background' style={{ padding: 0 }} />
+        <Header className='site-layout-background' style={{ padding: 0, position: 'fixed', zIndex: 1, width: '100%', float: 'right' }}>
+          <Menu mode='horizontal'>
+            <Menu.Item key='1'>Log out</Menu.Item>
+          </Menu>
+        </Header>
         <Content style={{ margin: '0 16px' }}>
           <Breadcrumb style={{ margin: '16px 0' }}>
             <Breadcrumb.Item>Patients</Breadcrumb.Item>
