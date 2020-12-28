e46932afb88a5080f9976a4c3787b5604fffbfb4
Sika Abarca <sikabarca@gmail.com>
2020-12-28 09:26:35 -0500

    Display full name of patient in details page

--- src/admin/PatientDetails.js
+++ src/admin/PatientDetails.js
@@ -1,5 +1,21 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
+import { Typography } from 'antd'
 
-const PatientDetails = () => <h1>Patient details XYZ</h1>
+const { Title } = Typography
+
+const getFullName = patient => `${patient.firstName} ${patient.middleName} ${patient.lastName} ${patient.secondLastName}`
+
+const PatientDetails = () => {
+  const location = useLocation()
+  const patient = location.state.patient
+  return (
+    <>
+      <Title level={2}>
+        { getFullName(patient) }
+      </Title>
+    </>
+  )
+}
 
 export default PatientDetails
